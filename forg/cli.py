57b967659a1d0816a34454dd5d22cef2d3276e9e
nordenxgt <nordenghisingt123@gmail.com>
2026-08-25 23:37:38 +0545

    fix: folder to directory, error handling, tuple constant

--- forg/cli.py
+++ forg/cli.py
@@ -4,34 +4,45 @@ import os
 import shutil
 from pathlib import Path
 
+FILE_ORGANIZATION = ("Documents", "Images", "Audios", "Videos")
+
 def ofile(directory):
-    directory = Path(directory)
+    if not directory.exists(): raise FileNotFoundError(f"The directory '{directory}' does not exist.")
+    if not directory.is_dir(): raise NotADirectoryError(f"'{directory}' is not a valid directory.")
+
     dir_contents = list(os.walk(directory))
-    if directory.exists():
-        for f in ["Documents", "Images", "Audios", "Videos"]:
-            (directory/f).mkdir(exist_ok=True)
+
+    for f in FILE_ORGANIZATION: 
+        (directory/f).mkdir(exist_ok=True)
+
     for _, _, filenames in dir_contents:
         for filename in filenames:
-            if filename.lower().endswith(".pdf"):
-                shutil.move(filename, (directory/"Documents"))
-            if filename.lower().endswith(".jpg"):
-                shutil.move(filename, (directory/"Images"))
+            if filename.lower().endswith(".txt"):
+                shutil.move((directory/filename), (directory/FILE_ORGANIZATION[0]))
+            elif filename.lower().endswith(".jpg"):
+                shutil.move((directory/filename), (directory/FILE_ORGANIZATION[1]))
             elif filename.lower().endswith(".mp3"):
-                shutil.move(filename, (directory/"Audios"))
+                shutil.move((directory/filename), (directory/FILE_ORGANIZATION[2]))
             elif filename.lower().endswith(".mp4"):
-                shutil.move(filename, (directory/"Videos"))
-        
+                shutil.move((directory/filename), (directory/FILE_ORGANIZATION[3]))
 
 def main():
     parser = argparse.ArgumentParser(description="File organizer CLI tool", usage="forg command")
     subparsers = parser.add_subparsers(dest="command", required=True)
+
     subparser_run = subparsers.add_parser("run", help="Run the file organization")
-    subparser_run.add_argument("folder", help="Path to the directory/directory")
+    subparser_run.add_argument("directory", help="Path to the directory")
     subparser_run.add_argument("-o", "--organize", type=str, choices=["file", "name", "date", "size"], default="file", help="Organization strategy of the files.")
+
     args = parser.parse_args()
+
     if args.command == "run":
         if args.organize == "file":
-            ofile(args.folder)
+            try: 
+                ofile(Path(args.directory))
+            except (FileNotFoundError, NotADirectoryError) as e: 
+                print(f"Error: {e}", file=sys.stderr)
+                sys.exit(2)
         elif args.organize == "name":
             pass
         elif args.organize == "date":
