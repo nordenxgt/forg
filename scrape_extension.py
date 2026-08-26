eb15daac60bc72708fcd2f490e07a48fb7c06625
nordenxgt <nordenghisingt123@gmail.com>
2026-08-27 00:07:37 +0545

    fix: change the json format to have k,v pair for each extensions, folder

--- scrape_extension.py
+++ scrape_extension.py
@@ -2,36 +2,32 @@ import json
 import bs4
 import requests
 
-url = "https://fileinfo.com/filetypes/common"
+URL = "https://fileinfo.com/filetypes/common"
 
-try:
-    response = requests.get(url)
-    response.raise_for_status()
-except requests.RequestException as e:
-    print(e)
-    raise
+def main():
+    try:
+        response = requests.get(URL)
+        response.raise_for_status()
+    except requests.RequestException as e:
+        print(e)
+        raise
 
-soup = bs4.BeautifulSoup(response.text, "html.parser")
+    soup = bs4.BeautifulSoup(response.text, "html.parser")
+    heading_elements = soup.select("h2")
+    extension_elements = soup.select(".extcol")
+    headings = tuple(heading.get_text().removesuffix(" Files") for heading in heading_elements)
 
-heading_elements = soup.select("h2")
-extension_elements = soup.select(".extcol")
+    i, data = 0, {}
+    for extension_element in extension_elements[1:]:
+        extension = extension_element.get_text().lower()
+        if extension == "extension":
+            i += 1
+        else: 
+            data[extension] = headings[i]
 
-headings = tuple(heading.get_text().removesuffix(" Files") for heading in heading_elements)
+    with open("extensions.json", "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=2)
+        print("Scraping successfull")
 
-current_group, extension_groups = [], []
-
-for extension_element in extension_elements:
-    extension_text = extension_element.get_text()
-    if extension_text == "Extension":
-        if current_group:
-            extension_groups.append(tuple(current_group))
-            current_group = []
-    else:
-        current_group.append(extension_text)
-if current_group:
-    extension_groups.append(tuple(current_group))
-
-extension_dict = dict(zip(headings, extension_groups))
-with open("extensions.json", "w", encoding="utf-8") as f:
-    json.dump(extension_dict, f, indent=2)
-    print("Scraping successfull")
+if __name__ == "__main__":
+    main()
